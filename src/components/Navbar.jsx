e584129f5fc0dc31496e2230873fc4b103861ec0
unknown <matheus.duarte42@gmail.com>
2023-06-10 12:21:29 -0300

    search-button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 import './Navbar.css'
 
 const Navbar = () => {
+    const [search, setSearch] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!search) return
+
+        navigate(`/search?q=${search}`)
+        setSearch('')
+    }
+
     return (
         <div>
             <nav className="navbar">
@@ -12,8 +24,8 @@ const Navbar = () => {
                         <BiCameraMovie /> MoviesLib
                     </Link>
                 </h2>
-                <form>
-                    <input type="text" placeholder='Busque um filme' />
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Busque um filme' onChange={(e) => setSearch(e.target.value)} value={search}/>
                     <button type='submit'>
                         <BiSearchAlt2 />
                     </button>
